9dde416712636e97b60ef666e23e9e34605fc6a2
Konstantin Gredeskoul <kigster@gmail.com>
2026-04-03 14:52:23 -0700

    Fixing post-rebase issues

--- gomoku-c/src/vendor/json-c/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kig/workspace/gomokus/gomoku-ansi-c.2/gomoku-c/src/vendor/json-c
-# Build directory: /Users/kig/workspace/gomokus/gomoku-ansi-c.2/gomoku-c/src/vendor/json-c/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
